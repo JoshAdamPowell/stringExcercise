4855fe94619bde014584986c0175b727ee592d1d
Josh Powell <josh.powell@softwire.com>
2018-02-20 14:34:03 +0000

    Excercise 1 complete

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-var csvFile = fs.readFileSync('‪C:\\Work\\training2\\strings\\bookcsv.csv').toString();
+var Sugar = require('sugar/date');
+var csvFile = fs.readFileSync('bookcsv.csv', 'utf8');
 var csvLines = csvFile.split(/\r?\n/);
 var initialString = "| Pub Date    |                         Title | Authors               |";
 var secondString = "|=====================================================================|";
-csvLines.forEach(function (line) {
-    var params = line.split(',');
+console.log(initialString);
+console.log(secondString);
+for (var i = 1; i < csvLines.length; i++) {
+    var params = csvLines[i].split(',');
     var date = params[0];
     var title = params[1];
     var authors = params[2];
-});
+    Sugar.Date.getLocale('en').addFormat('{dd}/{mm}/{yyyy}');
+    var newSugarDate = Sugar.Date.create(date);
+    var formattedDate = Sugar.Date.format(newSugarDate, '{dd} {Mon} {yyyy}');
+    if (title.length > 29) {
+        var shortTitle = title.substring(0, 26);
+        title = shortTitle + '...';
+    }
+    else {
+        for (var j = title.length; j < 29; j++) {
+            title = " " + title;
+        }
+    }
+    if (authors.length > 21) {
+        var shortAuthors = authors.substring(0, 18);
+        authors = shortAuthors + '...';
+    }
+    else {
+        for (var j = authors.length; j < 21; j++) {
+            authors = " " + authors;
+        }
+    }
+    console.log("| " + formattedDate + " | " + title + " | " + authors + " |");
+}
--- index.ts
+++ index.ts
@@ -1,16 +1,40 @@
 import * as fs from 'fs';
+var Sugar = require('sugar/date');
 
-let csvFile = fs.readFileSync('‪C:\\Work\\training2\\strings\\bookcsv.csv').toString();
+let csvFile = fs.readFileSync('bookcsv.csv', 'utf8');
 let csvLines = csvFile.split(/\r?\n/);
 let initialString = "| Pub Date    |                         Title | Authors               |";
 let secondString = "|=====================================================================|";
+console.log(initialString);
+console.log(secondString);
 
-csvLines.forEach((line)=> {
-    let params = line.split(',');
+for (let i = 1; i < csvLines.length; i++){
+    let params = csvLines[i].split(',');
     let date = params[0];
     let title = params[1];
     let authors = params[2];
 
+    Sugar.Date.getLocale('en').addFormat('{dd}/{mm}/{yyyy}');
+    let newSugarDate = Sugar.Date.create(date);
+    let formattedDate = Sugar.Date.format(newSugarDate,'{dd} {Mon} {yyyy}');
+    if (title.length > 29) {
+        let shortTitle = title.substring(0, 26);
+        title = shortTitle + '...';
+    } else {
+        for (let j = title.length; j < 29; j++){
+            title = " " + title
+        }
+    }
 
+    if (authors.length > 21) {
+        let shortAuthors = authors.substring(0, 18);
+        authors = shortAuthors + '...';
+    } else {
+        for (let j = authors.length; j < 21; j++){
+            authors = " " + authors;
+        }
+    }
 
-});
+    console.log(`| ${formattedDate} | ${title} | ${authors} |`);
+
+}
